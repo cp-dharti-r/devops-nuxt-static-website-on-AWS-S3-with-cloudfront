9ae7e32376b0750c90af6c745ae00f86427ac0c2
cp-dharti-r <dharti.r@canopas.com>
2024-11-18 12:25:04 +0530

    Configure tailwind css

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div style="text-align: center; padding: 20px 0; font-size: 20px">
+    <div
+      class="dark:bg-black dark:text-white text-center py-10 text-xl border-b dark:border-gray-500"
+    >
       <b>Dharti Ramoliya</b>: Practical 8 - Deploy Nuxt static website on AWS S3
       with Cloudfront
     </div>
